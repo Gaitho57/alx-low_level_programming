d6b76c3f80afdc827debe2bb23ad18d6f6e5367d
Gaitho57 <michaelgaitho10@gmail.com>
2023-02-20 01:13:21 -0800

    4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description: 'print alphabets expect q and e'
+ * Description: 'print alphabets except q and e'
  * Return: Always 0
  */
 int main(void)
 {
 	int n = 97;
 
-	while (n = 122)
+	while (n <= 122)
 	{
 		if (n == 101 || n == 113)
 		{
