784788720d17f1ce6438dfd7b5a4076f8580baa9
Gaitho57 <michaelgaitho10@gmail.com>
2023-04-09 22:30:14 -0700

    binary_to_unit_updt

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -5,7 +5,7 @@
  * binary_to_uint - converts a binary number to unsigned int
  * @b: String containing the binary number
  *
- * Return:An unsigned int the conerted number
+ * Return: the conerted number
  */
 
 unsigned int binary_to_uint(const char *b)
@@ -14,13 +14,15 @@ unsigned int binary_to_uint(const char *b)
 	unsigned int decimal_val = 0;
 
 	if (!b)
-		return (0);
+        return 0;
 
-	for (index = 0; b[index]; index++)
-	{
-		if (b[index] < '0' || b[index] > '1')
-			return (o);
-		decimal_val = 2 * decimal_val + (b[index] - '0');
-	}
-	return (decimal_val);
+    for (int index = 0; b[index]; index++)
+    {
+        if (b[index] < '0' || b[index] > '1')
+            return 0;
+        decimal_val = (decimal_val << 1) + (b[index] - '0');
+    }
+
+    return decimal_val;
 }
+
