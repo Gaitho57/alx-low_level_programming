2daca654355db3db4c8c1f79ac08c05d7c83d648
Gaitho57 <michaelgaitho10@gmail.com>
2023-04-09 23:45:10 -0700

    print_binary

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,18 +7,23 @@
  */
 void print_binary(unsigned long int n)
 {
-	int i;
+int i;
+int cnt = 0;
+unsigned long int present;
 
-	if (n == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	for (i = sizeof(n) * 8 - 1; i >= 0; i--)
-	{
-		if ((n >> i) & 1)
-			_putchar('1');
-		else
-			_putchar('0');
-	}
+for (i = 63; i >= 0; i--)
+{
+present = n >> i;
+
+if (present & 1)
+{
+_putchar('1');
+cnt++;
+}
+else if (cnt)
+_putchar('0');
+
+}
+if (!cnt)
+_putchar('0');
 }
