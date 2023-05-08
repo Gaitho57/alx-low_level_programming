b15166974d942b739720d1c12994b70a58621c50
Gaitho57 <michaelgaitho10@gmail.com>
2023-05-08 15:35:25 -0700

    A_P_TT_F

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * append_text_to_file - Adds text to the file terminal
+ * @filename: Apointer to the id of the file
+ * @text_content: The string to place at the end of the file
+ *
+ * Return: if function is not possible or fails filename is NULL - -1
+ * if the file is non existant the user lacks write permissions - -1
+ * incase - 1
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int file_descriptor, bytes_written, text_length = 0;
+
+	if (filename == NULL)
+		return (-1);
+
+	if (text_content != NULL)
+	{
+		for (text_length = 0; text_content[text_length];)
+			text_length++;
+}
+file_descriptor = open(filename, O_WRONLY | O_APPEND);
+bytes_written = write(file_descriptor, text_content, text_length);
+
+if (file_descriptor == -1 || bytes_written == -1)
+return (-1);
+
+close(file_descriptor);
+return (1);
+}
