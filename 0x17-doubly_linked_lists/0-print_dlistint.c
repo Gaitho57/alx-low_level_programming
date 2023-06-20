e5c2b74949b40ef8b937df5034a0a231961d3432
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-20 01:54:27 -0700

    update

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,23 +10,23 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-        int count = 0;
+int count = 0;
 
-        /* Check if the list is empty */
-        if (h == NULL)
-                return (count);
+/* Check if the list is empty */
+if (h == NULL)
+return (count);
 
-        /* Traverse the list from the head to the first node */
-        while (h->prev != NULL)
-                h = h->prev;
+/* Traverse the list from the head to the first node */
+while (h->prev != NULL)
+h = h->prev;
 
-        /* Print each node and update the count */
-        while (h != NULL)
-        {
-                printf("%d\n", h->n);
-                count++;
-                h = h->next;
-        }
+/* Print each node and update the count */
+while (h != NULL)
+{
+printf("%d\n", h->n);
+count++;
+h = h->next;
+}
 
-        return (count);
+return (count);
 }
