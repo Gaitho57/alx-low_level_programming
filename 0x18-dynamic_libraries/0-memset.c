93be3d62569542b1f4bfd066583b31b99240a963
Gaitho57 <michaelgaitho10@gmail.com>
2023-06-23 13:29:40 -0700

    update

---
+++ 0x18-dynamic_libraries/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _memset -adds memory with constant values
+ * @s: points to char params
+ * @b: constant byte type
+ * @n: byte of memory area
+ * Return: *s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+	s[i] = b;
+	}
+	return (s);
+}
